ad08703e0496be111d598298a62cf7405dbea3ef
Glen Roberts <glensroberts@gmail.com>
2016-01-08 18:29:40 -0500

    Test

--- async/weather.js
+++ async/weather.js
@@ -1,26 +1,26 @@
 var request = require('request');
 
-module.exports = function(location, callback) {
-	var encodedLocation = encodeURIComponent(location);
-	var url = 'http://api.openweathermap.org/data/2.5/weather?q=+ city' + encodedLocation + '&appid=87a7692ad27bc7aadbd5290b17202084&units=imperial';
-	
-	if(!location) {
-		return callback('No location provided');
-	}
+module.exports = function (location, callback) {
+    var encodedLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=+ city' + encodedLocation + '&appid=87a7692ad27bc7aadbd5290b17202084&units=imperial';
 
-	request({url: url,json: true}, function(error, response, body){
-		if(error) {
-			callback('Unable to fetch weather.');
-		} else {
-			var sunrise = new Date(body.sys.sunrise * 1000);
-			var sunset = new Date(body.sys.sunset * 1000);
-			var convertedSunset = sunset.getHours();
-			if(convertedSunset > 12) {
-				convertedSunset -= 12;
-			}
-			callback('The temperature is ' + body.main.temp + 'F in ' + body.name + '\n' +
-			'The sun will rise at ' + sunrise.getHours() + ':' + sunrise.getMinutes() + 'am' + '\n' +
-			'The sun will set at ' + convertedSunset + ':' + sunset.getMinutes() + 'pm');
-		}
-	});
+    if (!location) {
+        return callback('No location provided');
+    }
+
+    request({url: url, json: true}, function (error, response, body) {
+        if (error) {
+            callback('Unable to fetch weather.');
+        } else {
+            var sunrise = new Date(body.sys.sunrise * 1000);
+            var sunset = new Date(body.sys.sunset * 1000);
+            var convertedSunset = sunset.getHours();
+            if (convertedSunset > 12) {
+                convertedSunset -= 12;
+            }
+            callback('The temperature is ' + body.main.temp + 'F in ' + body.name + '\n' +
+                'The sun will rise at ' + sunrise.getHours() + ':' + sunrise.getMinutes() + 'am' + '\n' +
+                'The sun will set at ' + convertedSunset + ':' + sunset.getMinutes() + 'pm');
+        }
+    });
 }
