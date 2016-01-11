65984d2fe973e1b9d8c91829f80ac00d8d381be3
Glen Roberts <glensroberts@gmail.com>
2016-01-11 18:23:02 -0500

    Refactor middleware

---
+++ web-server/middleware.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private Route Hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+module.exports = middleware;
